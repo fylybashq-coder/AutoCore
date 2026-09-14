a5dbc5ff287995e9b5ba71cb04180ef6746d2f4b
fylybashq-coder <fylybashq@gmail.com>
2026-09-14 14:04:22 +0300

    Update appointment.py

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Date, Time, ForeignKey
 from sqlalchemy.orm import relationship
 
-from app.database import Base
+from database import Base
 
 
 class Appointment(Base):
